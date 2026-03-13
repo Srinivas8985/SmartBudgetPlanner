877e366d558eaa7ce983c1ca74e65989b74bbe40
Srinivas8985 <goud1632@gmail.com>
2026-03-13 20:07:39 +0530

    all hosts server allowed again2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,24 @@
 //   allowedHosts: 'all'
 // }
 // })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true,
+//     allowedHosts: 'all'
+//   }
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    allowedHosts: 'all'
+    allowedHosts: [
+      'smart-budget-frontend-olkd.onrender.com'
+    ]
   }
 })
